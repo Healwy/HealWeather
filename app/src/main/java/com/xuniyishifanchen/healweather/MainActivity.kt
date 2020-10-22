aa5e5798c20f31c8f0efda3494dfb1740cc3bad2
Healwy <982973349@qq.com>
2020-10-23 01:07:06 +0800

    Organize the code

--- app/src/main/java/com/xuniyishifanchen/healweather/MainActivity.kt
+++ app/src/main/java/com/xuniyishifanchen/healweather/MainActivity.kt
@@ -5,10 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    companion object {
-        private const val TAG = "MainActivity"
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
